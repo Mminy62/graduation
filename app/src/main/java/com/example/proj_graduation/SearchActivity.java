9a1b28527506c50582e841ce93849a25bb39d090
Mingso KIM <37893327+mingso@users.noreply.github.com>
2022-03-10 17:01:53 +0900

    [feat] 검색 인터페이스 추가

---
+++ app/src/main/java/com/example/proj_graduation/SearchActivity.java
@@ -0,0 +1,33 @@
+package com.example.proj_graduation;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    public static com.example.proj_graduation.SearchActivity sa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    public void doMySearch(String str) {
+
+    }
+
+
+}
