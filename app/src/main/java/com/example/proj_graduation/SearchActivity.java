c7a1cfe37d4d18812795dea48a3fe9b5ed5da29a
mingso <mpolicest@g.skku.edu>
2022-03-10 21:18:07 +0900

    [feat] 검색창 생성

--- app/src/main/java/com/example/proj_graduation/SearchActivity.java
+++ app/src/main/java/com/example/proj_graduation/SearchActivity.java
@@ -1,22 +1,48 @@
 package com.example.proj_graduation;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SearchRecentSuggestionsProvider;
 import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 public class SearchActivity extends AppCompatActivity {
-
     public static com.example.proj_graduation.SearchActivity sa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) findViewById(R.id.search_view);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                // 검색 버튼 누를 때 호출
+                Log.i("ming", "query" + query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                // 검색창에서 글자가 변경이 일어날 때마다 호출
+                Log.i("ming", "newText" + newText);
+
+                return true;
+            }
+        });
 
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
@@ -26,8 +52,6 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void doMySearch(String str) {
-
+        Log.i("mingming", "doMySearch: " + str);
     }
-
-
 }
