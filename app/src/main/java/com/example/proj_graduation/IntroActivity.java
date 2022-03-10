7de76e24ed53fe953a17889888d949ba4a989e95
mingso <mpolicest@g.skku.edu>
2022-03-10 21:27:07 +0900

    [feat] intro 화면에서 search화면으로 연결

--- app/src/main/java/com/example/proj_graduation/IntroActivity.java
+++ app/src/main/java/com/example/proj_graduation/IntroActivity.java
@@ -37,7 +37,7 @@ public class IntroActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
                 startActivity(intent);
             }
 
