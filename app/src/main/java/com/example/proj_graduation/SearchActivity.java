be8df9d86e1fe80eec3c79f43291bc2b326b9789
mingso <mpolicest@g.skku.edu>
2022-03-15 11:16:01 +0900

    [refactor] 불필요한 코드 제거

--- app/src/main/java/com/example/proj_graduation/SearchActivity.java
+++ app/src/main/java/com/example/proj_graduation/SearchActivity.java
@@ -3,48 +3,29 @@ package com.example.proj_graduation;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SearchRecentSuggestionsProvider;
 import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
 import android.util.ArraySet;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class SearchActivity extends AppCompatActivity {
-    public static com.example.proj_graduation.SearchActivity sa;
     ArraySet<ListViewItem> list;
     ListViewAdapter adapter;
     ListView listView;
 
-    private AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            ListViewItem lv = (ListViewItem) adapterView.getAdapter().getItem(i);
-            Toast.makeText(getApplicationContext(), lv.getTitle(), Toast.LENGTH_SHORT).show();
-            lv.getTitle();
+    private AdapterView.OnItemClickListener itemClickListener = (adapterView, view, i, l) -> {
+        ListViewItem lv = (ListViewItem) adapterView.getAdapter().getItem(i);
+        Toast.makeText(getApplicationContext(), lv.getTitle(), Toast.LENGTH_SHORT).show();
+        lv.getTitle();
 
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivityForResult(intent,1);
-        }
-
-        ;
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intent,1);
     };
 
     @Override
@@ -57,19 +38,6 @@ public class SearchActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.list_view);
 
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//                // 검색 버튼 누를 때 호출
-//                return true;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                // 검색창에서 글자가 변경이 일어날 때마다 호출
-//                return true;
-//            }
-//        });
 
         configureList();
         configureIntent();
@@ -106,22 +74,6 @@ public class SearchActivity extends AppCompatActivity {
             SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
                     SuggestionProvider.AUTHORITRY, SuggestionProvider.MODE);
             suggestions.saveRecentQuery(query, null);
-            doMySearch(query);
         }
-    }
-
-    public void doMySearch(String query) {
-//        List<String> result = list.stream()
-//                .filter(str -> str.trim().contains(query))
-//                .collect(Collectors.toList());
-
-//        if (adapter.isEmpty()) {
-//            Toast.makeText(SearchActivity.this, "No Match", Toast.LENGTH_SHORT).show();
-//        } else {
-//            for (String res: result) {
-//                adapter.getFilter().filter(res);
-////                adapter.in
-//            }
-//        }
     }
 }
