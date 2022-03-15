d4b89b4d2f20de272cc4e7cf9bbcab3fa1379480
mingso <mpolicest@g.skku.edu>
2022-03-15 11:27:45 +0900

    [feat] 검색 상세 화면 임시 추가

--- app/src/main/java/com/example/proj_graduation/SearchDetailActivity.java
+++ app/src/main/java/com/example/proj_graduation/SearchDetailActivity.java
@@ -1,7 +1,62 @@
 package com.example.proj_graduation;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SearchRecentSuggestions;
+import android.util.ArraySet;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 
 public class SearchDetailActivity extends AppCompatActivity {
+    ArraySet<ListViewItem> list;
+    ListViewAdapter adapter;
+    ListView listView;
 
+    private AdapterView.OnItemClickListener itemClickListener = (adapterView, view, i, l) -> {
+        ListViewItem lv = (ListViewItem) adapterView.getAdapter().getItem(i);
+        Toast.makeText(getApplicationContext(), lv.getTitle(), Toast.LENGTH_SHORT).show();
+        lv.getTitle();
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intent,1);
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_detail);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        listView = (ListView) findViewById(R.id.list_view);
+
+        configureList();
+    }
+
+    private void configureList() {
+        list = new ArraySet<ListViewItem>();
+        list.add(new ListViewItem(ContextCompat.getDrawable(this, R.drawable.ic_arrow),
+                "서울 중앙 고등학교",
+                "연수와 웅의 고등학생 시절"));
+        list.add(new ListViewItem(ContextCompat.getDrawable(this, R.drawable.ic_arrow),
+                "수원 화성",
+                "피크닉 데이트"));
+        list.add(new ListViewItem(ContextCompat.getDrawable(this, R.drawable.ic_arrow),
+                "논산 온빛 자연휴양림",
+                "납치되어 갔던 곳"));
+        list.add(new ListViewItem(ContextCompat.getDrawable(this, R.drawable.ic_arrow),
+                "고인돌 공원",
+                "소나기를 피하던 장소"));
+
+        adapter = new ListViewAdapter();
+        adapter.addItems(list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(itemClickListener);
+    }
 }
