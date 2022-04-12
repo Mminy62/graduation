11521cd1e5d2a319ee840687e8f0fec8246a44e6
mingso <mpolicest@g.skku.edu>
2022-04-12 12:14:46 +0900

    [feat] 장소 이미지 추가

--- app/src/main/java/com/example/proj_graduation/SearchDetailActivity.java
+++ app/src/main/java/com/example/proj_graduation/SearchDetailActivity.java
@@ -6,16 +6,18 @@ import android.os.Parcelable;
 import android.util.ArraySet;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import org.w3c.dom.Text;
+
 
 public class SearchDetailActivity extends AppCompatActivity {
     ArraySet<SpotListViewItem> list;
     ListViewAdapter adapter;
     ListView listView;
+    TextView textView;
     Spot[] spots;
 
     private AdapterView.OnItemClickListener itemClickListener = (adapterView, view, i, l) -> {
@@ -31,14 +33,17 @@ public class SearchDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_detail);
 
+        listView = (ListView) findViewById(R.id.list_view);
+        textView = (TextView) findViewById(R.id.title_textView);
+
         Intent intent = getIntent();
         Parcelable[] parcels = intent.getParcelableArrayExtra("spots");
+        textView.setText(intent.getStringExtra("name"));
         spots = new Spot[parcels.length];
         for (int i= 0; i < parcels.length; i++){
             spots[i] = (Spot) parcels[i];
         }
 
-        listView = (ListView) findViewById(R.id.list_view);
 
         configureList();
     }
@@ -46,7 +51,8 @@ public class SearchDetailActivity extends AppCompatActivity {
     private void configureList() {
         list = new ArraySet<>();
         for (int i= 0; i < spots.length; i++) {
-            list.add(new SpotListViewItem("https://ww.namu.la/s/0ec826b267ef49b1aca68a00ebec19ef4b0f719378d1ae6dad1dcb03902358753ca1b7b702622850d6b603256cfe2d5fb292e8a52c6b96721c44ee5190e52f2df1977b0e5f7584f91491946be4da64858096a20ba356cd81da9a44eeb9b30435",
+            list.add(new SpotListViewItem(
+                    spots[i].getImageURL(),
                     spots[i].getName(),
                     spots[i].getDesc(),
                     spots[i].getLatitude(),
